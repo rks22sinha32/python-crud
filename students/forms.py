1daecc230ce17b30031589c509bed316838695e9
rks22sinha32 <limcarahul@gmail.com>
2026-02-17 01:27:18 +0530

    Update forms.py

--- students/forms.py
+++ students/forms.py
@@ -4,4 +4,4 @@ from .models import Student
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['name', 'email', 'mobile', 'address1']
+        fields = ['name', 'email', 'mobile', 'address']
